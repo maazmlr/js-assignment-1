3c397c371cdce474e662f370c804f900113e1131
maazmlr <75994722+maazmlr@users.noreply.github.com>
2023-01-28 13:59:43 +0500

    Add files via upload

---
+++ js 1/app.js
@@ -0,0 +1,7 @@
+//alert("Hello how r y doing ")
+//alert("Error ! please Enter a valid password.");
+//alert("Welcome to js land \n happy coding")
+// alert("welcome to js land")
+// alert("happy coding")
+
+alert("hello ... can i run js")
